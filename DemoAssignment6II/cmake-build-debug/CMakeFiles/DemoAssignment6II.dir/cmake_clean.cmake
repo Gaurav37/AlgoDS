56903e7b854c447abf777f519f9c606b11a6c746
Gaurav Singh <Gauravsingh4293@gmail.com>
2022-03-25 14:57:05 -0400

    Add files via upload

---
+++ DemoAssignment6II/cmake-build-debug/CMakeFiles/DemoAssignment6II.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DemoAssignment6II.dir/main.cpp.obj"
+  "DemoAssignment6II.exe"
+  "DemoAssignment6II.exe.manifest"
+  "DemoAssignment6II.pdb"
+  "libDemoAssignment6II.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DemoAssignment6II.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
